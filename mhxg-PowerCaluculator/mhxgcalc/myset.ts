44f5e4331ceb9e20a9b280b83f63969d77bdf0b6
gitnsy <jump24d@gmai.com>
2016-01-10 19:47:26 +0900

    マイセットクラスだけ追加。やりたいことが思い出せる範囲は書いた

---
+++ mhxg-PowerCaluculator/mhxgcalc/myset.ts
@@ -0,0 +1,13 @@
+﻿module mhxgcalc {
+
+    /**
+     * マイセット。装備とかスキルとか飯護符情報とか。
+     */
+    export class Myset {
+        /** 武器 */
+        weapon: Weapon;
+        /** スキル */
+        skillSet: Skill[];
+        /** 飯護符などその他 */
+    }
+}
